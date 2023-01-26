6d9508890cd97ad2aa451fc7aedad24430c0efc2
Peter Walsh <peter.t.walsh@stonybrook.edu>
2023-01-26 15:05:01 -0500

    Removed event types from event type enum.

--- src/hw2/HW2Events.ts
+++ src/hw2/HW2Events.ts
@@ -10,32 +10,11 @@ export const HW2Events = {
 	PLAYER_MINE_COLLISION: "PLAYER_MINE_COLLISION", 
 
     /**
-     * The event that gets emitted when the player collides with a bubble
-     * 
-     * Has data: {}
-     */
-	PLAYER_BUBBLE_COLLISION: "PLAYER_BUBBLE_COLLISION",
-
-    /**
-     * The event that gets emitted when the player's health changes
-     * 
-     * Has data: { curhp: number, maxhp: number }
-     */
-    HEALTH_CHANGE: "HEALTH_CHANGE",
-
-    /**
      * The event that gets emitted when the charge of the player's laser beam changes
      * 
      * Has data: { curchrg: number, maxchrg: number }
      */
     CHARGE_CHANGE: "CHARGE_CHANGE",
-
-    /**
-     * The event that gets emitted when the amount of air the player has left changes
-     * 
-     * Has data: { curair: number, maxair: number }
-     */
-	AIR_CHANGE: "AIR_CHANGE",
 
     /**
      * The event that gets emitted when the player's health hits 0 (or minhealth)
@@ -60,17 +39,6 @@ export const HW2Events = {
      */
     FIRING_LASER: "LASER_FIRING",
 
-    /**
-     * The event sent by a mines animation manager after the mine has played it's exploding animation
-     * 
-     * Has data: { id: number, }
-     * 
-     */
-    MINE_EXPLODED: "MINE_EXPLODED",
-
-
     LASER_MINE_COLLISION: "LASER_MINE_COLLISION",
-
-    PLAYER_WAS_HIT: "PLAYER_WAS_HIT"
     
 } as const;
